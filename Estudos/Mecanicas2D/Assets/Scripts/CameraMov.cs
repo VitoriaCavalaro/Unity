b2f2c76855e0eb9fcd1d45b673b7fb02e85b747c
Heyviics <45526503+Heyviics@users.noreply.github.com>
2020-07-14 21:43:09 -0300

    camera follow

---
+++ Estudos/Mecanicas2D/Assets/Scripts/CameraMov.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour
+{
+
+    public Transform alvo;
+    public Vector3 posicaoFinal;
+    public Vector3 Offset;
+
+    void LateUpdate()
+    {
+        posicaoFinal = alvo.position;
+
+        transform.position = posicaoFinal + Offset;
+    }
+}
